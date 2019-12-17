a9a874e7f0e1f0b9aec4a77e618eee27a0fc428b
larsberglen <lars.berglen@ub.gu.se>
2019-12-17 10:47:10 +0100

    Accessibility blurbs and shortcuts (#88)
    * fixes
    
    * accessibility focus on blurb

--- web/sites/default/modules/features/ubn_news/ubn_news.js
+++ web/sites/default/modules/features/ubn_news/ubn_news.js
@@ -7,6 +7,21 @@
     }
   };
 
+
+   Drupal.behaviors.ubn_news_accessibility = {
+    attach: function(context, settings) {
+      $(document).keydown(function(e){
+        if(e.keyCode == 9){
+         $('.promoted-wrapper').addClass('show-focus-outlines');
+        }
+      });
+
+      $(document).on('mousedown', function(){
+        $('.promoted-wrapper').removeClass('show-focus-outlines');
+      });
+    }
+  };
+
   Drupal.behaviors.ubn_news_blurb_mouse = {
     attach: function(context, settings) {
       $('.promoted-wrapper').on('mouseenter mouseleave', function(){
--- web/sites/default/modules/features/ubn_startpage/ubn_startpage.js
+++ web/sites/default/modules/features/ubn_startpage/ubn_startpage.js
@@ -20,6 +20,20 @@
     }
   };
 
+  Drupal.behaviors.ubn_startpage_accessibility = {
+    attach: function(context, settings) {
+      $(document).keydown(function(e){
+        if(e.keyCode == 9){
+         $('.blurb-wrapper').addClass('show-focus-outlines');
+        }
+      });
+
+      $(document).on('mousedown', function(){
+        $('.blurb-wrapper').removeClass('show-focus-outlines');
+      });
+    }
+  };
+
   Drupal.behaviors.ubn_startpage_main_blurb_mouse = {
     attach: function(context, settings) {
       $('.blurb-wrapper').on('mouseenter mouseleave', function(){
