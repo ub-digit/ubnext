7d1a4517f6982147878fa80efa1dcc21f826ca82
David Gustafsson <glasklas@gmail.com>
2021-06-18 13:40:46 +0200

    Set release branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :repo_url, 'https://github.com/ub-digit/ubnext.git'
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
-set :branch, 'ubnext-1229-printing-css'
+set :branch, 'upload-field-add-allowed-filetype-extensions'
 
 # Default deploy_to directory is /var/www/my_app
 # set :deploy_to, '/var/www/drupal/staging'
