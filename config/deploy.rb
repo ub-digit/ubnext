73dfd4ac12dcc7ec511443f31c8acc7bb1a89cb7
Lars Berglén <lars.berglen@ub.gu.se>
2022-12-05 14:19:07 +0100

    new deploy branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :repo_url, 'https://github.com/ub-digit/ubnext.git'
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
-set :branch, 'patch-for-fieldset-bug-in-chrome'
+set :branch, 'release-2022.11.001'
 
 # Default deploy_to directory is /var/www/my_app
 # set :deploy_to, '/var/www/drupal/staging'
