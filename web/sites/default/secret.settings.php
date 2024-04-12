b596326b318badfca27cae2bf1236f8af695575b
David Gustafsson <david.gustafsson@ub.gu.se>
2024-04-12 14:51:08 +0200

    Use environment variables in settings files

---
+++ web/sites/default/secret.settings.php
@@ -0,0 +1,20 @@
+<?php
+$databases = array (
+  'default' =>
+   array (
+     'default' =>
+      array (
+       'database' => 'drupal7_ubnext',
+       'username' => 'drupal7_ubnext',
+       'password' => 'drupal7_ubnext',
+       'host' => 'database',
+       'port' => '',
+       'driver' => 'mysql',
+       'prefix' => '',
+     ),
+   ),
+);
+
+$drupal_hash_salt = getenv('UBN_DRUPAL_HASH_SALT');
+$conf['ubn_summon_access_id'] = getenv('UBN_SUMMON_ACCESS_ID');
+$conf['ubn_summon_secret_key'] = getenv('UBN_SUMMON_SECRET_KEY');
---
+++ web/sites/default/site.settings.php
@@ -0,0 +1,6 @@
+<?php
+$conf['file_temporary_path'] = '/tmp';
+$conf['ubn_settings_solr_host'] = 'solr';
+$conf['slate_cache_enabled'] = getenv('UBN_SLATE_CACHE_ENABLED');
+$conf['ubn_terms_cache'] = getenv('UBN_TERMS_CACHE_ENABLED');
+
