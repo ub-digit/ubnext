9466959901d4ce15e1168f195ccf2505707e129b
David Gustafsson <david.gustafsson@ub.gu.se>
2024-04-16 15:39:40 +0200

    Add google key setting

--- web/sites/default/secret.settings.php
+++ web/sites/default/secret.settings.php
@@ -18,3 +18,4 @@ $databases = array (
 $drupal_hash_salt = getenv('UBN_DRUPAL_HASH_SALT');
 $conf['ubn_summon_access_id'] = getenv('UBN_SUMMON_ACCESS_ID');
 $conf['ubn_summon_secret_key'] = getenv('UBN_SUMMON_SECRET_KEY');
+$conf['ubn_google_key'] = getenv('UBN_GOOGLE_KEY');
